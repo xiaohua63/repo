4499ed1f1d35dfefd57cdb4b3ef2fcea4a2188db
A先生 <142523398+axs66@users.noreply.github.com>
2024-10-06 15:08:54 +0800

    Update generate_packages.sh

--- generate_packages.sh
+++ generate_packages.sh
@@ -18,8 +18,20 @@ ls -l "$DEB_DIR"
 
 # Check if there are any .deb files in the DEB_DIR
 if ls "$DEB_DIR"/*.deb 1> /dev/null 2>&1; then
+  # 先备份原来的 Packages 文件
+  if [ -f Packages ]; then
+    cp Packages Packages.bak
+  fi
+
   # Generate the Packages file
   dpkg-scanpackages -m "$DEB_DIR" > Packages
+
+  # 将备份的内容合并到新生成的 Packages 文件中
+  if [ -f Packages.bak ]; then
+    cat Packages.bak >> Packages
+    sort -u Packages -o Packages  # 去重
+    rm Packages.bak  # 删除备份文件
+  fi
 
   # Compress the Packages file
   bzip2 -fks Packages
